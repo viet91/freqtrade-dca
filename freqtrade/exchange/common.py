051c4f1c5b89fa4ab046ef93c6a9e30cfc4f5a41
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2021-11-01 15:11:47 +0900

    Update common.py

--- freqtrade/exchange/common.py
+++ freqtrade/exchange/common.py
@@ -57,7 +57,6 @@ def calculate_backoff(retrycount, max_retries):
     """
     return (max_retries - retrycount) ** 2 + 1
 
-
 def retrier_async(f):
     async def wrapper(*args, **kwargs):
         count = kwargs.pop('count', API_RETRY_COUNT)
@@ -70,14 +69,39 @@ def retrier_async(f):
                 count -= 1
                 kwargs.update({'count': count})
                 if isinstance(ex, DDosProtection):
-                    backoff_delay = calculate_backoff(count + 1, API_RETRY_COUNT)
-                    logger.info(f"Applying DDosProtection backoff delay: {backoff_delay}")
-                    await asyncio.sleep(backoff_delay)
+                    if "kucoin" in str(ex) and "429000" in str(ex):
+                        logger.warning(f"Kucoin 429 error, keeping count the same.")
+                        count += 1
+                    else:
+                        backoff_delay = calculate_backoff(count + 1, API_RETRY_COUNT)
+                        logger.info(f"Applying DDosProtection backoff delay: {backoff_delay}")
+                        await asyncio.sleep(backoff_delay)
                 return await wrapper(*args, **kwargs)
             else:
                 logger.warning('Giving up retrying: %s()', f.__name__)
                 raise ex
     return wrapper
+    
+# def retrier_async(f):
+#     async def wrapper(*args, **kwargs):
+#         count = kwargs.pop('count', API_RETRY_COUNT)
+#         try:
+#             return await f(*args, **kwargs)
+#         except TemporaryError as ex:
+#             logger.warning('%s() returned exception: "%s"', f.__name__, ex)
+#             if count > 0:
+#                 logger.warning('retrying %s() still for %s times', f.__name__, count)
+#                 count -= 1
+#                 kwargs.update({'count': count})
+#                 if isinstance(ex, DDosProtection):
+#                     backoff_delay = calculate_backoff(count + 1, API_RETRY_COUNT)
+#                     logger.info(f"Applying DDosProtection backoff delay: {backoff_delay}")
+#                     await asyncio.sleep(backoff_delay)
+#                 return await wrapper(*args, **kwargs)
+#             else:
+#                 logger.warning('Giving up retrying: %s()', f.__name__)
+#                 raise ex
+#     return wrapper
 
 
 def retrier(_func=None, retries=API_RETRY_COUNT):
